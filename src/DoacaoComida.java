a72d958206f38dbc5bc268060b937ee9e61b8191
Anderzoum Lopes <andersonqueirogap@gmail.com>
2026-04-24 00:09:08 -0300

    first commit

---
+++ src/DoacaoComida.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DoacaoComida {
+
+
+    private static int id;
+    private String descricaoDoacao;
+    private String statusDoacao;
+    private String estabelecimento;
+    private double quantidadeKg;
+    private String regiao;
+
+
+    public DoacaoComida(String estabelecimento, String descricaoDoacao, String statusDoacao, String regiao, double quantidadeKg) {
+        id++;
+        this.descricaoDoacao = descricaoDoacao;
+        this.estabelecimento = estabelecimento;
+        this.statusDoacao = statusDoacao;
+        this.regiao = regiao;
+        this.quantidadeKg = quantidadeKg;
+    }
+
+    /*FILTRO POR REGIAO*/
+    public static List<DoacaoComida> filtrarPorRegiao (List<DoacaoComida> doacao, String regiao){
+        return doacao.stream()
+                .filter(d -> d.getRegiao().equalsIgnoreCase(regiao))
+                .collect(Collectors.toList());
+
+    }
+
+    /*VIZUALIZAR DOAÇÃO DISPONIVEL*/
+    public static List<DoacaoComida> filtrarPorStatus (List<DoacaoComida> doacao, String status){
+        return doacao.stream()
+                .filter(d -> d.getStatusDoacao().equalsIgnoreCase(status))
+                .collect(Collectors.toList());
+
+    }
+
+
+
+
+    public String getDescricaoDoacao() {
+        return descricaoDoacao;
+    }
+
+    public void setDescricaoDoacao(String descricaoDoacao) {
+        this.descricaoDoacao = descricaoDoacao;
+    }
+
+    public String getStatusDoacao() {
+        return statusDoacao;
+    }
+
+    public void setStatusDoacao(String statusDoacao) {
+        this.statusDoacao = statusDoacao;
+    }
+
+    public String getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(String estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    public double getQuantidadeKg() {
+        return quantidadeKg;
+    }
+
+    public void setQuantidadeKg(double quantidadeKg) {
+        this.quantidadeKg = quantidadeKg;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    static void main(String[] args) {
+
+
+        List<DoacaoComida> todasDoacoes = new ArrayList<>();
+
+        todasDoacoes.add (new DoacaoComida("Mix Matheus", "Arroz e feijao", "Disponivel" , "Centro", 15));
+        todasDoacoes.add(new DoacaoComida("Padaria Bom sucesso", "Pães", "Disponível", "Centro", 3.0));
+        todasDoacoes.add(new DoacaoComida("Sacolao", "Frutas", "Pendente", "Jatoba", 7.2));
+
+        List<DoacaoComida> filtroCentro = DoacaoComida.filtrarPorRegiao(todasDoacoes, "centro");
+
+
+
+
+    }
+
+}
